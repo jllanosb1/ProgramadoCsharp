81eaaa57baa399f9f1bb2638097dd21a5122fad5
Ronal <altamiranoronal2005@gmail.com>
2025-09-09 08:11:57 -0500

    Agregando mi codigo dde estudiante N00407287

---
+++ Participantes/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Participantes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Estudiantes de aula nrc 6414");
+            Console.WriteLine(" N00407287");
+
+        }
+    }
+}
