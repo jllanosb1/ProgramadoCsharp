eba9bd822f313876ba1f0faee82bbd234edf80b3
Luis <N00476231@upn.pe>
2025-09-09 08:12:05 -0500

    agregando mi codigo de estudiante N00470620

---
+++ participantes/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace participantes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("estudiantes de la clase NRC 6414");
+            Console.WriteLine("N00470620");
+        }
+    }
+}
